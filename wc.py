34598275145ecbad3b031152e4f9fd3955e76469
kaku13745107 <kakuhrf@yahoo.co.jp>
2013-06-09 20:15:39 +0900

    wc.py

--- wc.py
+++ wc.py
@@ -1,19 +1,42 @@
 import sys
+import re
 
+"""
 f = sys.stdin
 s = f.read()
 f.close()
+add start
+"""
+filename = sys.argv[1]
 
+try:
+    f = open(filename,'rU')
+except IOError:
+    print >> sys.stderr, "Error:open file failed!"
+    sys.exit()
+
+
+"""
 words = s.split()
-
-d = {}
-
-for w in words: 
+for w in words:
     if d.has_key(w):
         d[w] += 1
-    else: 
+    else:
         d[w] = 1
-
+line = f.readline()
+"""
+d = {}
+r = re.compile(r"[,|.|'|\"|!|?]")
+line = f.readline()
+while line:
+    line = r.sub("", line)
+    for w in line.split():
+        w = w.lower()
+        if d.has_key(w):
+            d[w] += 1
+        else:
+            d[w] = 1
+    line = f.readline()
 
 sored_keys = sorted(d.keys(), key = lambda x: d[x], reverse = True)
 print "all: %d" % len(d)
@@ -24,6 +47,5 @@ for k in sored_keys:
         break
     print k, ": ", d[k]
     i += 1
-
 
 
